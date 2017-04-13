284ed6f7bb2bc14af16c3e6130f2acfc7d271e5b
Noah Nordrum <noah.nordrum@optum.com>
2017-04-13 16:43:50 -0500

    added support for a system property-configured additional hook.

--- src/main/java/org/terracotta/jenkins/plugins/postcompleted/PostCompletedPlugin.java
+++ src/main/java/org/terracotta/jenkins/plugins/postcompleted/PostCompletedPlugin.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
  * @author Anthony Dahanne
  */
 public class PostCompletedPlugin extends Plugin {
-    private final static Logger LOG = Logger.getLogger(PostCompletedPlugin.class.getName());
+    private final Logger LOG = Logger.getLogger(getClass().getName());
 
     public void start() throws Exception {
         LOG.info("Starting PostCompletedPlugin");
